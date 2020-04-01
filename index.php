ce434be70aa068b883f1654bd526456139f62989
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-01 19:45:23 -0300

    Adicionando código funcional

--- index.php
+++ index.php
@@ -1,31 +1,40 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" type="text/css" href="DataTables-1.10.20/css/dataTables.bootstrap4.min.css" />
-    <link rel="stylesheet" type="text/css" href="Buttons-1.6.1/css/buttons.bootstrap4.min.css" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.1/css/buttons.dataTables.min.css">
 
-    <script type="text/javascript" src="JSZip-2.5.0/jszip.min.js"></script>
-    <script type="text/javascript" src="pdfmake-0.1.36/pdfmake.min.js"></script>
-    <script type="text/javascript" src="pdfmake-0.1.36/vfs_fonts.js"></script>
-    <script type="text/javascript" src="DataTables-1.10.20/js/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="DataTables-1.10.20/js/dataTables.bootstrap4.min.js"></script>
-    <script type="text/javascript" src="Buttons-1.6.1/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" src="Buttons-1.6.1/js/buttons.bootstrap4.min.js"></script>
-    <script type="text/javascript" src="Buttons-1.6.1/js/buttons.html5.min.js"></script>
-    <script type="text/javascript" src="Buttons-1.6.1/js/buttons.print.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.flash.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.1/js/buttons.print.min.js"></script>
 
 
-    <title>Testando implementação DataTable</title>
+    <script>
+        $(document).ready(function() {
+            $('#example').DataTable({
+                dom: 'Bfrtip',
+                buttons: [
+                    'copy', 'csv', 'excel', 'pdf', 'print'
+                ]
+            });
+        });
+    </script>
+
+    <title>Document</title>
 </head>
 
 <body>
 
-    <h1>Teste de implementação do plug-in DataTable</h1>
-    <br><br>
 
     <table id="example" class="display nowrap" style="width:100%">
         <thead>
