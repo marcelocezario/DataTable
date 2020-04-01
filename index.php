198193e1e53d2a070d115f9d4749530aae3e258a
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-01 18:43:14 -0300

    Adicionando imports no html

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="DataTables-1.10.20/css/dataTables.bootstrap4.min.css" />
+    <link rel="stylesheet" type="text/css" href="Buttons-1.6.1/css/buttons.bootstrap4.min.css" />
+
+    <script type="text/javascript" src="JSZip-2.5.0/jszip.min.js"></script>
+    <script type="text/javascript" src="pdfmake-0.1.36/pdfmake.min.js"></script>
+    <script type="text/javascript" src="pdfmake-0.1.36/vfs_fonts.js"></script>
+    <script type="text/javascript" src="DataTables-1.10.20/js/jquery.dataTables.min.js"></script>
+    <script type="text/javascript" src="DataTables-1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script type="text/javascript" src="Buttons-1.6.1/js/dataTables.buttons.min.js"></script>
+    <script type="text/javascript" src="Buttons-1.6.1/js/buttons.bootstrap4.min.js"></script>
+    <script type="text/javascript" src="Buttons-1.6.1/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" src="Buttons-1.6.1/js/buttons.print.min.js"></script>
+
+
     <title>Testando implementação DataTable</title>
 </head>
+
 <body>
 
-<h1>Teste de implementação do plug-in DataTable</h1>
-<br><br>
-    
+    <h1>Teste de implementação do plug-in DataTable</h1>
+    <br><br>
+
 </body>
+
 </html>
