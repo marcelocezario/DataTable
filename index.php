edbcfca73c07785a7ef35b942991675ff0a72aea
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-01 10:28:25 -0300

    Criando html base para testes

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testando implementação DataTable</title>
+</head>
+<body>
+
+<h1>Teste de implementação do plug-in DataTable</h1>
+<br><br>
+    
+</body>
+</html>
