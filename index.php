eb69efc0024577e67377910c92e5a518583cec00
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-02 18:01:34 -0300

    Tirando botão de exportar para csv

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
             $('#example').DataTable({
                 dom: 'Bfrtip',
                 buttons: [
-                    'copy', 'csv', 'excel', 'pdf', 'print'
+                    'copy', 'excel', 'pdf', 'print'
                 ]
             });
         });
